d056665f7aa09c437b5ae43987264cd446587ec2
s-oronae <s-oronae@52L145-02.it.bsd405.org>
2019-10-23 13:53:54 -0700

    Initial Commit

---
+++ src/main/java/com/robototes/RobototesLibrary.java
@@ -0,0 +1,19 @@
+package com.robototes;
+
+/**
+ * A class that stores most of the information about the library.
+ *
+ * @author Eli Orona
+ * @version 0.1
+ */
+public class RobototesLibrary {
+	private static String version = "0.0.1";
+
+	/**
+	 * 
+	 * @return The current version of the library
+	 */
+	public static String getVersion() {
+		return version;
+	}
+}
