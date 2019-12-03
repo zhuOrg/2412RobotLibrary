b1c9d6d7378f2bef31ec81e430b719f69c75e5da
s-oronae <s-oronae@52S500041226.it.bsd405.org>
2019-12-03 11:52:21 -0800

    Adding in test case for library

---
+++ src/test/java/com/robototes/RobototesLibraryTest.java
@@ -0,0 +1,14 @@
+package com.robototes;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class RobototesLibraryTest {
+
+	@Test
+	public void testGetVersion() {
+		assertEquals("Get current version of library", "0.0.1", RobototesLibrary.getVersion());
+	}
+
+}
